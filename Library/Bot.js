11c472bd1994713a4a7fc575f481f28de4bac73b
deaaprizal <afrizaldea94@gmail.com>
2024-02-15 11:41:33 +0700

    first commit

---
+++ Library/Bot.js
@@ -0,0 +1,43 @@
+let TelegramBot = require("node-telegram-bot-api");
+
+class Bot extends TelegramBot {
+    constructor(token) {
+        super(token, { polling: true })
+    }
+    receive_sticker() {
+        this.on("sticker", (callback) => {
+            const id = callback.from.id
+            const sticker = callback.sticker.emoji
+            this.sendMessage(id, sticker)
+
+        })
+    }
+    earth_quake() {
+        this.onText(/!gempa/, async (data) => {
+            const id = data.from.id
+            const url = "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json"
+            const api = await fetch(url)
+            const response = await api.json()
+            const { Kedalaman, Magnitude, Wilayah, Potensi, Tanggal, Jam } = response.Infogempa.gempa
+            const result = `info gempa terbaru:\n\n${Tanggal} | ${Jam}\nWilayah: ${Wilayah}\nBesar: ${Magnitude} SR\nKedalaman: ${Kedalaman}\nPotensi: ${Potensi}`
+
+            this.sendMessage(id, result)
+        })
+    }
+    get_profile() {
+        this.onText(/!myprofile/, (data, after) => {
+            console.log(after)
+            const { id, first_name, last_name, username } = data.from;
+            const response = `username ${username}, nama lengkap ${first_name} ${last_name}`;
+            this.sendMessage(id, response);
+        });
+    }
+    follow_me() {
+        this.onText(/!followme(.+)/, (data, after) => {
+            let chatId = data.from.id;
+            this.sendMessage(chatId, after[1]);
+        });
+    }
+}
+
+module.exports = Bot
---
+++ main.js
@@ -0,0 +1,20 @@
+const env = require("dotenv")
+const Bot = require("./Library/Bot")
+env.config()
+
+// BOT CONFIG
+const bot_token = process.env.token
+
+// INITIALIZING BOT
+const bot = new Bot(bot_token)
+
+// ACTIVATE BOT FEATURE
+bot.receive_sticker()
+bot.get_profile()
+bot.follow_me()
+bot.earth_quake()
+
+
+
+
+
