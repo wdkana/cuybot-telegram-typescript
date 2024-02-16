15daafb503ec8b46398bfeb782b870a15a418e68
deaaprizal <afrizaldea94@gmail.com>
2024-02-16 12:41:39 +0700

    add: try&catch block, new init function

---
+++ Library/commands.js
@@ -0,0 +1,12 @@
+const commands = {
+    quake: /!quake\b/,
+    profile: /!profile\b/,
+    quote: /!quote\b/,
+    news: /!news\b/,
+    help: /!help\b/,
+    podcast: /!podcast\b/,
+    followme: /!followme(.+)/,
+    avatar: /!avatar(.+)/,
+}
+
+module.exports = commands
