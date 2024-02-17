c46f3aa921310dfa989d4acebc488df729f090b5
deaaprizal <afrizaldea94@gmail.com>
2024-02-17 20:03:36 +0700

    refacto: add global command_sign

--- libs/commands.js
+++ libs/commands.js
@@ -1,21 +1,20 @@
 const command_sign = "!"
 
-function regex(word, type) {
+function filtered(word, type) {
     if (type == "after") {
         return new RegExp(`^${command_sign}${word}(.+)`)
-    } else {
-        return new RegExp(`^${command_sign}${word}$`)
     }
+    return new RegExp(`^${command_sign}${word}$`)
 }
 
 const commands = {
-    quake: regex("quake"),
-    profile: regex("profile"),
-    quote: regex("quote"),
-    news: regex("news"),
-    help: regex("help"),
-    followme: regex("followme", "after"),
-    avatar: regex("avatar", "after"),
+    quake: filtered("quake"),
+    profile: filtered("profile"),
+    quote: filtered("quote"),
+    news: filtered("news"),
+    help: filtered("help"),
+    followme: filtered("followme", "after"),
+    avatar: filtered("avatar", "after"),
 }
 
 module.exports = commands
