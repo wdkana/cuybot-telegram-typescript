37cc4483f78c65621fdda8166775fc6c8d1ab2e2
deaaprizal <afrizaldea94@gmail.com>
2024-02-17 19:59:14 +0700

    fix: commands utils

--- libs/commands.js
+++ libs/commands.js
@@ -1,11 +1,21 @@
+const command_sign = "!"
+
+function regex(word, type) {
+    if (type == "after") {
+        return new RegExp(`^${command_sign}${word}(.+)`)
+    } else {
+        return new RegExp(`^${command_sign}${word}$`)
+    }
+}
+
 const commands = {
-    quake: /^!quake$/,
-    profile: /^!profile$/,
-    quote: /^!quote$/,
-    news: /^!news$/,
-    help: /^!help$/,
-    followme: /^!followme(.+)/,
-    avatar: /^!avatar(.+)/,
+    quake: regex("quake"),
+    profile: regex("profile"),
+    quote: regex("quote"),
+    news: regex("news"),
+    help: regex("help"),
+    followme: regex("followme", "after"),
+    avatar: regex("avatar", "after"),
 }
 
 module.exports = commands
