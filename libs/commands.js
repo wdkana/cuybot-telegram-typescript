7c910bc5e74ef926a9ba8e2d258f302b7e38fc63
deaaprizal <afrizaldea94@gmail.com>
2024-02-17 19:48:02 +0700

    fix: regex for commands

--- libs/commands.js
+++ libs/commands.js
@@ -1,12 +1,11 @@
 const commands = {
-    quake: /!quake\b/,
-    profile: /!profile\b/,
-    quote: /!quote\b/,
-    news: /!news\b/,
-    help: /!help\b/,
-    podcast: /!podcast\b/,
-    followme: /!followme(.+)/,
-    avatar: /!avatar(.+)/,
+    quake: /^!quake$/,
+    profile: /^!profile$/,
+    quote: /^!quote$/,
+    news: /^!news$/,
+    help: /^!help$/,
+    followme: /^!followme(.+)/,
+    avatar: /^!avatar(.+)/,
 }
 
 module.exports = commands
