e79ecbeb2cf36d23e7d8879b841817e2fb92cf59
deaaprizal <afrizaldea94@gmail.com>
2024-03-01 13:27:20 +0700

    shadow: update

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
 import Bot from "./src/app/Bot";
 
-process.env.TZ = 'Indonesia/Jakarta';
+process.env.TZ = "Indonesia/Jakarta";
 
-require("dotenv").config()
+require("dotenv").config();
 
 type BotOptions = {
-    polling: boolean;
-    filePath?: boolean;
+  polling: boolean;
+  filePath?: boolean;
 };
 
-const token: string | undefined = process.env.TOKEN
-const botOptions: BotOptions = { polling: true, filePath: false }
+// ...
 
-const bot = new Bot(token, botOptions)
+const token: string | undefined = process.env.TOKEN;
+const botOptions: BotOptions = { polling: true, filePath: false };
+
+const bot = new Bot(token, botOptions);
 
 function main(): void {
-    bot.showHelp()
-    bot.showFollowedText()
-    bot.showAvatar()
-    bot.showEarthQuakeInfo()
-    bot.showGreeting()
-    bot.showNews()
-    bot.showQuotes()
+  bot.showHelp();
+  bot.showFollowedText();
+  bot.showAvatar();
+  bot.showEarthQuakeInfo();
+  bot.showGreeting();
+  bot.showNews();
+  bot.showQuotes();
 }
 
-console.log('Bot is running now!')
+console.log("Bot is running now!");
 
-main()
+main();
+
